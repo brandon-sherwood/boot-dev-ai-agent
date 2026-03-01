54bd4e42868f4923a7aa8ddfc2bd02179f1d7bde
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-01 07:24:19 -0700

    Created token usage counters. Moved logic into main function.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
 )
-print(response.text)
 
 
 def main():
@@ -17,6 +16,16 @@ def main():
 
     if api_key == None:
         raise RuntimeError("Environment variable not found!")
+
+    if response.usage_metadata == None:
+        raise RuntimeError("Failed API request")
+
+    X = response.usage_metadata.prompt_token_count
+    Y = response.usage_metadata.candidates_token_count
+
+    print(f"Prompt tokens: {X}")
+    print(f"Response tokens: {Y}")
+    print(response.text)
 
 
 if __name__ == "__main__":
