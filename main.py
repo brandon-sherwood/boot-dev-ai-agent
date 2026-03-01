c7bf4a7d0cda2faca13115fca4388582b9af86b6
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-01 08:04:13 -0700

    Remove hard coded prompt. Added user prompt.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User Prompt")
+args = parser.parse_args()
+user_input = args.user_prompt
+
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+    model='gemini-2.5-flash', contents=user_input
 )
 
 
