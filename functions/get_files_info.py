98380a1446be6f2a9931e5cd86d4cf8fd77f98e2
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-03 07:20:48 -0700

    Implemented try/except block to catch errors. Created test file in root.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,24 +2,28 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    abs_path = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(abs_path, directory))
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_path, directory))
 
-    valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
-    if valid_target_dir == False:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        valid_target_dir = os.path.commonpath(
+            [abs_path, target_dir]) == abs_path
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
 
-    accum_results = []
-    for i in os.listdir(target_dir):
-        full_path = os.path.join(target_dir, i)
-        file_size = os.path.getsize(full_path)
-        is_dir = os.path.isdir(full_path)
-        accum_results.append(
-            f"- {i}: file_size={file_size} bytes, is_dir={is_dir}")
+        accum_results = []
+        for i in os.listdir(target_dir):
+            full_path = os.path.join(target_dir, i)
+            file_size = os.path.getsize(full_path)
+            is_dir = os.path.isdir(full_path)
+            accum_results.append(
+                f"- {i}: file_size={file_size} bytes, is_dir={is_dir}")
 
-    final_output = "\n".join(accum_results)
+        final_output = "\n".join(accum_results)
 
-    return final_output
+        return final_output
+    except Exception as e:
+        return f"Error: {e}"
