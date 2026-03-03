d9f6a4732729af06d7201f1ce9ddfe8a38bcd3b7
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-03 07:15:33 -0700

    Created accumulated results into final_output.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,3 +8,18 @@ def get_files_info(working_directory, directory="."):
     valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
     if valid_target_dir == False:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    accum_results = []
+    for i in os.listdir(target_dir):
+        full_path = os.path.join(target_dir, i)
+        file_size = os.path.getsize(full_path)
+        is_dir = os.path.isdir(full_path)
+        accum_results.append(
+            f"- {i}: file_size={file_size} bytes, is_dir={is_dir}")
+
+    final_output = "\n".join(accum_results)
+
+    return final_output
