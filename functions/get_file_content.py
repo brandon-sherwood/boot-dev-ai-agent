2355027ece8a5623a5a79b1864c7db364449e723
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-04 07:07:52 -0700

    Created get_file_content.py under fucntions directory. Created error strings.

---
+++ functions/get_file_content.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    abs_path = os.path.abspath(working_directory)
+    target_file = os.path.normpath(os.path.join(abs_path, file_path))
+
+    valid_target_path = os.path.commonpath([abs_path, target_file]) == abs_path
+    if valid_target_path == False:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
