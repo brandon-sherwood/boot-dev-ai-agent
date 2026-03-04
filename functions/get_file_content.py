7d9ed09df05eac16fdb796295c1220e4c356a701
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-04 11:03:42 -0700

    Created config.py to hold max characters variable.  Read and return file as a string set up. Errors raised in try/ except block.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,12 +2,26 @@ import os
 
 
 def get_file_content(working_directory, file_path):
-    abs_path = os.path.abspath(working_directory)
-    target_file = os.path.normpath(os.path.join(abs_path, file_path))
 
-    valid_target_path = os.path.commonpath([abs_path, target_file]) == abs_path
-    if valid_target_path == False:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_path, file_path))
 
-    if not os.path.isfile(target_file):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
+        valid_target_path = os.path.commonpath(
+            [abs_path, target_file]) == abs_path
+        if valid_target_path == False:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_file):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return file_content_string
+
+    except Exception as e:
+        return f"Error: {e}"
