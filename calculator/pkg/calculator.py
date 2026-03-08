b849847a5de8059ad5a2d8c9ed2a30e18389a7eb
brandon-sherwood <sherwood.brandonlee@gmail.com>
2026-03-08 06:50:08 -0700

    Intentionally altered the precedence of operators to test agent's ability to alter files. Agent readjusted operators in calculator/pkg/calculator.py.

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -9,10 +9,10 @@ class Calculator:
             "/": lambda a, b: a / b,
         }
         self.precedence = {
-            "+": 1,
+            "*": 3,
+            "/": 3,
+            "+": 2,
             "-": 1,
-            "*": 2,
-            "/": 2,
         }
 
     def evaluate(self, expression):
